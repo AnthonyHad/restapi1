6a4bbfd471a9dc9c06ead656ebfa7b5e98699a09
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-02-24 08:59:03 +0100

    added validations and enriched controller methods

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,43 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first API post!' }],
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first API post!',
+        imageUrl: 'images/395497.jpg',
+        creator: {
+          name: 'Anthony',
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: 'Validation failed, data is incorrect',
+        errors: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
   // create post in DB
   res.status(201).json({
     message: 'Post created successfully !',
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: 'Anthony' },
+      createdAt: new Date(),
+    },
   });
 };
